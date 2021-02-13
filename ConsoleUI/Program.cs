39067d8bafb0a4b184e304c346f5960a626c8818
serdar tekin <kara_bulu@hotmail.com>
2021-02-14 01:16:15 +0300

    Proje dosyası ekle.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productmanager = new ProductManager(new InMemoryProductdal());
+
+            foreach (var p in productmanager.GetAll())
+            {
+                Console.WriteLine(p.ProductName);
+            }
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductdal.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductdal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductdal()
+        {
+            _products = new List<Product>{
+
+                new Product{ProdcutId=1, CategoryId=1, ProductName="Bardak", UnitPrice = 15, UnitsInStock =15 },
+                new Product{ProdcutId=2, CategoryId=1, ProductName="Kamera", UnitPrice = 500, UnitsInStock =3 },
+                new Product{ProdcutId=3, CategoryId=2, ProductName="Telefon", UnitPrice = 1500, UnitsInStock =2 },
+                new Product{ProdcutId=4, CategoryId=2, ProductName="Klavye", UnitPrice = 150, UnitsInStock =65 },
+                new Product{ProdcutId=5, CategoryId=2, ProductName="Fare", UnitPrice = 85, UnitsInStock =1 }
+
+            };
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProdcutId == product.ProdcutId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+        public void Delete(Product product)
+        {
+            Product productToDelete = _products.SingleOrDefault(p => p.ProdcutId == product.ProdcutId);
+            _products.Remove(productToDelete);
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+    }
+}
