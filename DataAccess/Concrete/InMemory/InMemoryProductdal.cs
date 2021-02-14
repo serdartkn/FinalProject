d9807900069edeede143c5ff479ee95ccec6a40c
serdar tekin <kara_bulu@hotmail.com>
2021-02-14 18:12:18 +0300

    Düzenleme

--- DataAccess/Concrete/InMemory/InMemoryProductdal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductdal.cs
@@ -14,15 +14,14 @@ namespace DataAccess.Concrete.InMemory
         {
             _products = new List<Product>{
 
-                new Product{ProdcutId=1, CategoryId=1, ProductName="Bardak", UnitPrice = 15, UnitsInStock =15 },
-                new Product{ProdcutId=2, CategoryId=1, ProductName="Kamera", UnitPrice = 500, UnitsInStock =3 },
-                new Product{ProdcutId=3, CategoryId=2, ProductName="Telefon", UnitPrice = 1500, UnitsInStock =2 },
-                new Product{ProdcutId=4, CategoryId=2, ProductName="Klavye", UnitPrice = 150, UnitsInStock =65 },
-                new Product{ProdcutId=5, CategoryId=2, ProductName="Fare", UnitPrice = 85, UnitsInStock =1 }
+                new Product{ProductId=1, CategoryId=1, ProductName="Bardak", UnitPrice = 15, UnitsInStock =15 },
+                new Product{ProductId=2, CategoryId=1, ProductName="Kamera", UnitPrice = 500, UnitsInStock =3 },
+                new Product{ProductId=3, CategoryId=2, ProductName="Telefon", UnitPrice = 1500, UnitsInStock =2 },
+                new Product{ProductId=4, CategoryId=2, ProductName="Klavye", UnitPrice = 150, UnitsInStock =65 },
+                new Product{ProductId=5, CategoryId=2, ProductName="Fare", UnitPrice = 85, UnitsInStock =1 }
 
             };
         }
-
         public List<Product> GetAll()
         {
             return _products;
@@ -33,7 +32,7 @@ namespace DataAccess.Concrete.InMemory
         }
         public void Update(Product product)
         {
-            Product productToUpdate = _products.SingleOrDefault(p => p.ProdcutId == product.ProdcutId);
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
             productToUpdate.ProductName = product.ProductName;
             productToUpdate.CategoryId = product.CategoryId;
             productToUpdate.UnitPrice = product.UnitPrice;
@@ -41,7 +40,7 @@ namespace DataAccess.Concrete.InMemory
         }
         public void Delete(Product product)
         {
-            Product productToDelete = _products.SingleOrDefault(p => p.ProdcutId == product.ProdcutId);
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
             _products.Remove(productToDelete);
         }
 
